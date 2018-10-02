46d9cdc6602394259bbb11268811559f3ab61303
Christine Joy Villaruel <cjpvillaruel@gmail.com>
2018-10-03 02:06:48 +0800

    Add less style preprocessor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Button from 'antd/lib/button';
 import './App.css';
+import Home from './components/Home'
 
 class App extends Component {
   render() {
@@ -8,9 +10,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Home />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Button type="primary">Button</Button>
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ src/components/Home/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import './home.less'
+
+const Home = () => <div> Hello </div>;
+export default Home;
